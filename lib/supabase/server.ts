3dc4a69835f865d36e9cc46c56756acc133012c2
amayagustavo91 <amayagustavo91@gmail.com>
2026-03-20 02:07:14 -0300

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,14 +1,15 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
+
 export function createClient() {
   const cookieStore = cookies()
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     { cookies: {
-      get: (name) => cookieStore.get(name)?.value,
-      set: (name, value, options) => cookieStore.set({ name, value, ...options }),
-      remove: (name, options) => cookieStore.set({ name, value: '', ...options }),
+      get: (name: string) => cookieStore.get(name)?.value,
+      set: (name: string, value: string, options: any) => cookieStore.set({ name, value, ...options }),
+      remove: (name: string, options: any) => cookieStore.set({ name, value: '', ...options }),
     }}
   )
 }
