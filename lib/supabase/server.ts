24375d7acfb9993ed4139df4e893cbd1a2525aec
amayagustavo91 <amayagustavo91@gmail.com>
2026-05-20 03:48:49 -0300

    Make createClient function async and update cookie handling
    Change createClient function to be asynchronous and await cookie store.

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,22 +1,24 @@
 import { createServerClient } from '@supabase/ssr'
 import { cookies } from 'next/headers'
 
-export function createClient() {
-  const cookieStore = cookies()
+export async function createClient() {
+  const cookieStore = await cookies()
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll(): { name: string; value: string }[] {
+        getAll() {
           return cookieStore.getAll()
         },
-        setAll(cookiesToSet: { name: string; value: string; options?: any }[]) {
+        setAll(cookiesToSet) {
           try {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             )
-          } catch {}
+          } catch {
+          }
         },
       },
     }
